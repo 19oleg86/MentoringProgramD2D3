5ad3a560f419c0eee7b1dfb883bdfa92cba6ea3c
Oleg Shulga <28oleg28@mail.ru>
2023-04-09 19:29:10 +0600

    Finished first task

--- MultiThreading.Task1.100Tasks/Program.cs
+++ MultiThreading.Task1.100Tasks/Program.cs
@@ -4,6 +4,8 @@
  * “Task #0 – {iteration number}”.
  */
 using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task1._100Tasks
 {
@@ -15,24 +17,35 @@ namespace MultiThreading.Task1._100Tasks
         static void Main(string[] args)
         {
             Console.WriteLine(".Net Mentoring Program. Multi threading V1.");
-            Console.WriteLine("1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.");
+            Console.WriteLine("1. Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.");
             Console.WriteLine("Each Task should iterate from 1 to 1000 and print into the console the following string:");
             Console.WriteLine("“Task #0 – {iteration number}”.");
             Console.WriteLine();
             
             HundredTasks();
-
+            
             Console.ReadLine();
         }
 
         static void HundredTasks()
         {
-            // feel free to add your code here
+            Task[] taskArray = new Task[100];
+            for (int i = 0; i < taskArray.Length; i++)
+            {
+                taskArray[i] = Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        Output(Task.CurrentId, j+1);
+                    }
+                });
+            }
+            Task.WaitAll(taskArray);
         }
 
-        static void Output(int taskNumber, int iterationNumber)
+        static void Output(int? taskNumber, int iterationNumber)
         {
-            Console.WriteLine($"Task #{taskNumber} – {iterationNumber}");
+            Console.WriteLine($"Task #{taskNumber} – {iterationNumber}; ");
         }
     }
 }
