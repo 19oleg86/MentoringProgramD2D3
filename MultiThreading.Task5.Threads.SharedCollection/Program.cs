6616f8e2f5d30752bda2a6c0ab2a85665e6a64e2
Oleg Shulga <28oleg28@mail.ru>
2023-04-12 14:36:22 +0600

    Finished fifth task

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,17 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        static List<int> collection = new List<int>();
+        static ManualResetEventSlim event1 = new ManualResetEventSlim(false);
+        static ManualResetEventSlim event2 = new ManualResetEventSlim(false);
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -18,8 +24,47 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine();
 
             // feel free to add your code
+            Thread thread1 = new Thread(AddElements) { Name = "Thread 1", };
+            Thread thread2 = new Thread(PrintElements) { Name = "Thread 2" };
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("Finished!");
 
             Console.ReadLine();
+        }
+
+        static void AddElements()
+        {
+            while (collection.Count <= 10)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    event1.Wait();
+                    collection.Add(i);
+                    Console.WriteLine($"{Thread.CurrentThread.Name} Added element {i} to the collection");
+                    event1.Reset();
+                    event2.Set();
+                }
+            }
+        }
+
+        static void PrintElements()
+        {
+            while (collection.Count <= 10)
+            {
+                Console.Write($"{Thread.CurrentThread.Name} prints collection: ");
+                for (int i = 0; i < collection.Count; i++)
+                {
+                    Console.Write($"{collection[i]}, ");
+                }
+                Console.WriteLine();
+                event1.Set();
+                event2.Wait();
+                event2.Reset();
+            }
         }
     }
 }
