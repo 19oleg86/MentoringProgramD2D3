b06281059cff815dace70356e9f40e6a10c1be47
Oleg Shulga <28oleg28@mail.ru>
2023-04-28 18:55:33 +0600

    Added DataCaptureService logic

--- Module03/DataCaptureService/DCService.cs
+++ Module03/DataCaptureService/DCService.cs
@@ -13,7 +13,7 @@ namespace DataCaptureService
         private const string DATACAPTUREDIRECTORY = "\\DataCaptureDirectory";
         static void Main(string[] args)
         {
-
+            CreateExchange();
             string workingDirectory = Environment.CurrentDirectory;
             string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
 
@@ -23,8 +23,9 @@ namespace DataCaptureService
                 watcher.Created += OnFileAdded;
                 watcher.EnableRaisingEvents = true;
 
-                Console.WriteLine("To start working with Data Capture Service add some files in \"DataCaptureDirectoty\". Pay attention that only .pdf files " +
-                                  "will be processed and send to the queue");
+                Console.WriteLine($"To start working with Data Capture Service add some files in \"DataCaptureDirectoty\". {Environment.NewLine} " +
+                    $"Pay attention that only .pdf files will be processed and send to the queue");
+
                 Console.WriteLine("Press enter to exit.");
                 Console.ReadLine();
             }
@@ -34,9 +35,34 @@ namespace DataCaptureService
         {
             if (e.FullPath.EndsWith(".pdf"))
             {
-                Console.WriteLine("Added file is pdf");
+                Console.WriteLine("Added file is in pdf format");
                 Console.WriteLine($"File's full name with path: {e.FullPath}");
+                var factory = new ConnectionFactory
+                {
+                    Uri = new Uri("amqp://guest:guest@localhost:5672")
+                };
+                var connection = factory.CreateConnection();
+                var channel = connection.CreateModel();
+                channel.ExchangeDeclare("webappExchange", ExchangeType.Direct, true);
+                byte[] fileBytes = File.ReadAllBytes(e.FullPath);
+                channel.BasicPublish("webappExchange", "file.pdf", null, fileBytes);
+
+                channel.Close();
+                connection.Close();
             }
+        }
+
+        private static void CreateExchange()
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("webappExchange", ExchangeType.Direct, true);
+            channel.Close();
+            connection.Close();
         }
     }
 }
