b706d2f0b61e8f8b82f8877e30cbe12358e4233f
Oleg Shulga <28oleg28@mail.ru>
2023-04-30 00:16:58 +0600

    Finished Module03 Task

--- Module03/DataCaptureService/DCService.cs
+++ Module03/DataCaptureService/DCService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DataCaptureService
@@ -11,6 +12,7 @@ namespace DataCaptureService
     class DCService
     {
         private const string DATACAPTUREDIRECTORY = "\\DataCaptureDirectory";
+        private const int HUNDRED_MB_IN_BYTES = 104857600;
         static void Main(string[] args)
         {
             CreateExchange();
@@ -33,8 +35,12 @@ namespace DataCaptureService
 
         private static void OnFileAdded(object sender, FileSystemEventArgs e)
         {
-            if (e.FullPath.EndsWith(".pdf"))
+            if (e.ChangeType == WatcherChangeTypes.Created && e.FullPath.EndsWith(".pdf"))
             {
+                string fileName = e.FullPath.Substring(e.FullPath.LastIndexOf('\\') + 1);
+                long fileSize = new FileInfo(e.FullPath).Length;
+
+                bool fileAvailable = false;
                 Console.WriteLine("Added file is in pdf format");
                 Console.WriteLine($"File's full name with path: {e.FullPath}");
                 var factory = new ConnectionFactory
@@ -43,10 +49,62 @@ namespace DataCaptureService
                 };
                 var connection = factory.CreateConnection();
                 var channel = connection.CreateModel();
-                channel.ExchangeDeclare("addFileExchange", ExchangeType.Direct, true);
-                byte[] fileBytes = File.ReadAllBytes(e.FullPath);
-                channel.BasicPublish("addFileExchange", "file.pdf", null, fileBytes);
+                channel.ExchangeDeclare("addFileExchange", ExchangeType.Headers, true);
+                while (!fileAvailable)
+                {
+                    try
+                    {
+                        byte[] fileBytes = File.ReadAllBytes(e.FullPath);
+                        fileAvailable = true;
 
+                        if (fileSize >= HUNDRED_MB_IN_BYTES)
+                        {
+                            Console.WriteLine($"Created file \"{e.Name}\" has 100 Mb or bigger size");
+                            var fileStream = new FileStream(e.FullPath, FileMode.Open, FileAccess.Read);
+                            var bigSizeheaders = new Dictionary<string, object>
+                            {
+                                { "fileName", fileName },
+                                { "size", "big" },
+                            };
+
+                            int chunkNumber = 1;
+                            byte[] buffer = new byte[52428800]; // 50MB
+                            while (fileStream.Position < fileStream.Length)
+                            {
+                                int bytesRead = fileStream.Read(buffer, 0, buffer.Length);
+                                byte[] chunkData = new byte[bytesRead];
+                                Array.Copy(buffer, chunkData, bytesRead);
+                                var bigSizeProperties = channel.CreateBasicProperties();
+                                bigSizeProperties.Headers = bigSizeheaders;
+                                if (!bigSizeProperties.Headers.ContainsKey("chunknumber"))
+                                    bigSizeProperties.Headers.Add("chunknumber", chunkNumber.ToString());
+                                bigSizeProperties.Headers["chunknumber"] = chunkNumber.ToString();
+
+                                channel.BasicPublish("addFileExchange", string.Empty, bigSizeProperties, chunkData);
+                                Console.WriteLine($"Sent chunk {chunkNumber}");
+                                chunkNumber++;
+                            }
+                            fileStream.Close();
+                        }
+                        else
+                        {
+                            var headers = new Dictionary<string, object>
+                            {
+                                { "fileName", fileName },
+                                { "size", "normal" }
+                            };
+                            var properties = channel.CreateBasicProperties();
+                            properties.Headers = headers;
+                            channel.BasicPublish("addFileExchange", string.Empty, properties, fileBytes);
+                        }
+
+                    }
+                    catch (IOException)
+                    {
+                        Console.WriteLine($"File {e.FullPath} is not available yet. Retrying in 1 second.");
+                        Thread.Sleep(1000); // wait for 1 second before trying again
+                    }
+                }
                 channel.Close();
                 connection.Close();
             }
@@ -60,7 +118,7 @@ namespace DataCaptureService
             };
             var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
-            channel.ExchangeDeclare("addFileExchange", ExchangeType.Direct, true);
+            channel.ExchangeDeclare("addFileExchange", ExchangeType.Headers, true);
             channel.Close();
             connection.Close();
         }
--- Module03/MainProcessingService/MPService.cs
+++ Module03/MainProcessingService/MPService.cs
@@ -17,28 +17,55 @@ namespace MainProcessingService
             string workingDirectory = Environment.CurrentDirectory;
             string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
             string filePath = $"{projectDirectory}{TARGETFILEDIRECTORY}\\";
-            var factory = new ConnectionFactory();
-            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
 
             var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
 
             channel.QueueDeclare("addFileQueue", true, false, false);
-            channel.QueueBind("addFileQueue", "addFileExchange", "file.pdf");
+            channel.QueueBind("addFileQueue", "addFileExchange", string.Empty);
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (sender, eventArgs) =>
             {
-                //var msg = System.Text.Encoding.UTF8.GetString(eventArgs.Body.ToArray()); eventArgs.Body.ToArray()
-                var fileBytes = eventArgs.Body.ToArray();
-                File.WriteAllBytes($"{filePath}file.pdf", fileBytes);
-                Console.WriteLine($"Exchange routing key: {eventArgs.RoutingKey}, Target file path: {filePath}file.pdf");
+                var fileName = Encoding.UTF8.GetString(eventArgs.BasicProperties.Headers["fileName"] as byte[]);
+                var fileSize = Encoding.UTF8.GetString(eventArgs.BasicProperties.Headers["size"] as byte[]);
+                if (fileSize == "normal")
+                {
+                    var fileBytes = eventArgs.Body.ToArray();
+                    File.WriteAllBytes($"{filePath}{fileName}", fileBytes);
+                    Console.WriteLine($"Target file path: {filePath}{fileName}");
+                    channel.BasicAck(eventArgs.DeliveryTag, false);
+                }
+                else if (fileSize == "big")
+                {
+                    var headers = eventArgs.BasicProperties.Headers;
+                    var chunkNumber = Encoding.UTF8.GetString(eventArgs.BasicProperties.Headers["chunknumber"] as byte[]);
+                    var chunkData = eventArgs.Body.ToArray();
+
+                    AppendChunkToFile(chunkData, int.Parse(chunkNumber), $"{filePath}{fileName}");
+                    channel.BasicAck(eventArgs.DeliveryTag, false);
+                }
             };
+
             channel.BasicConsume("addFileQueue", true, consumer);
 
+            Console.WriteLine("Press any key to exit.");
             Console.ReadLine();
 
             channel.Close();
             connection.Close();
+        }
+
+        private static void AppendChunkToFile(byte[] chunkData, int chunkNumber, string outputFile)
+        {
+            using (var fileStream = new FileStream(outputFile, chunkNumber == 1 ? FileMode.Create : FileMode.Append))
+            {
+                fileStream.Write(chunkData, 0, chunkData.Length);
+            }
+            Console.WriteLine($"Received chunk {chunkNumber}");
         }
     }
 }
