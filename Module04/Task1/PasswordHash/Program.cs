50aa5e0875a3da3735c767e2a503e8cfdbd10ecb
Oleg Shulga <28oleg28@mail.ru>
2023-05-06 20:07:24 +0600

    Finished first task

--- Module04/Task1/PasswordHash/Program.cs
+++ Module04/Task1/PasswordHash/Program.cs
@@ -7,10 +7,42 @@ namespace PasswordHash
     {
         static void Main(string[] args)
         {
+            string password = "myPassword123";
+            byte[] salt = GenerateSalt();
 
+            //Initial method version call
+            //string hashedPassword = GeneratePasswordHashUsingSalt(password, salt);
+            //Console.WriteLine($"Hashed Password: {hashedPassword}");
+
+            string hashedPassword = GeneratePasswordHashUsingSaltImproved(password, salt);
+            Console.WriteLine($"Hashed Password: {hashedPassword}");
         }
 
-        public string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+        public static byte[] GenerateSalt()
+        {
+            byte[] salt = new byte[16];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(salt);
+            }
+            return salt;
+        }
+
+        public static string GeneratePasswordHashUsingSaltImproved(string passwordText, byte[] salt)
+        {
+            const int saltSize = 16;  //Instead of separate hashBytes array with a fixed size of 36
+            const int hashSize = 20;  //Also constants instead of hardcoded values - better for code readability and maintainability
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterations: 10000);
+            byte[] hash = pbkdf2.GetBytes(hashSize);
+            var passwordHashBytes = new byte[saltSize + hashSize];
+            Buffer.BlockCopy(salt, 0, passwordHashBytes, 0, saltSize);          // More efficient copying the salt and hash into passwordHashBytes array
+            Buffer.BlockCopy(hash, 0, passwordHashBytes, saltSize, hashSize);   // in a single operation, comeparing to Array.Copy
+            var passwordHash = Convert.ToBase64String(passwordHashBytes);
+            return passwordHash;
+        }
+
+        //Initial method version
+        public static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
         {
             var iterate = 10000;
             var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
@@ -21,5 +53,6 @@ namespace PasswordHash
             var passwordHash = Convert.ToBase64String(hashBytes);
             return passwordHash;
         }
+        
     }
 }
