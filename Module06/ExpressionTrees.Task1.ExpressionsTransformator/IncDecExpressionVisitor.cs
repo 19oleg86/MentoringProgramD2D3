a26da66b2c48ac0d17f54ae5eea4f29cf029fef6
Oleg Shulga <28oleg28@mail.ru>
2023-05-21 12:48:27 +0600

    IncDecExpressionVisitor implementation and changed DotNet version

--- Module06/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
+++ Module06/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
@@ -5,5 +5,21 @@ namespace ExpressionTrees.Task1.ExpressionsTransformer
     public class IncDecExpressionVisitor : ExpressionVisitor
     {
         // todo: feel free to add your code here
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (node.Left.NodeType != ExpressionType.Parameter ||
+                node.Right is not ConstantExpression constant ||
+                constant.Value?.ToString() != "1")
+            {
+                return base.VisitBinary(node);
+            }
+
+            return node.NodeType switch
+            {
+                ExpressionType.Add => Expression.Increment(node.Left),
+                ExpressionType.Subtract => Expression.Decrement(node.Left),
+                _ => base.VisitBinary(node)
+            };
+        }
     }
 }
