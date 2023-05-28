3d2fe00f589798ff31d86458d7a1c5d1e3ec6d1a
Oleg Shulga <28oleg28@mail.ru>
2023-05-28 15:57:34 +0600

    Initial commit - added domain classes

---
+++ Module07/CatalogService/Categories.Domain/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Categories.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
